f37b4c5a5e96911032adceabfa3043f6a3a816bf
Ranga Rathnayake <58025781+RangaRathnayake@users.noreply.github.com>
2024-01-01 15:08:50 +0530

    cursor position change manually

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div style="width: 50%; margin: auto;">
-    <vue-tel-input v-model="phone" v-bind="bindProps"></vue-tel-input>
+    <vue-tel-input v-model="phone" v-bind="bindProps" v-on:keyup="inputEvent($event)"
+      @click="textClickEvent($event)"></vue-tel-input>
+  </div>
+  <div>
+    <br>
+    Tel: {{ phone }}<br>
+    <br>
+    before: {{ before }}<br>
+    <br>
+    positionChanged: {{ positionChanged }}<br>
   </div>
 </template>
 
@@ -15,12 +24,13 @@ export default {
   setup() {
     const phone = ref(null);
 
+
     return {
       phone: phone,
       bindProps: {
         mode: "international",
         defaultCountry: "LK",
-        autoFormat: true,
+        autoFormat: false,
         dropdownOptions: {
           showDialCodeInSelection: true,
           showSearchBox: true,
@@ -28,8 +38,44 @@ export default {
         }
       }
     }
+  },
+
+  data() {
+    return {
+      before: 0,
+      positionChanged: false,
+    }
+  },
+
+  methods: {
+    inputEvent(event) {
+
+      // console.log("defalut -> ", event.target.selectionStart);
+      // console.log("key Code ", event.keyCode);
+      if (event.keyCode == 37 || event.keyCode == 39) {
+        this.before = event.target.selectionStart;
+        this.positionChanged = true;
+        // console.log("set before -> ", this.before);
+      } else if (event.keyCode > 48 && event.keyCode < 58) {
+        if (this.positionChanged) {
+          // console.log("before +1 ", Number(this.before) + 1);
+          // console.log("selectionStart ", event.target.selectionStart)
+          event.target.setSelectionRange(Number(this.before) + 1, Number(this.before) + 1);
+          this.before = Number(this.before) + 1;
+        }
+      }
+    },
+
+    textClickEvent(event) {
+      console.log("textClickEvent");
+      console.log(event.target.selectionStart);
+      this.before = event.target.selectionStart;
+      this.positionChanged = true;
+    }
   }
+
 }
+
 
 </script>
 
